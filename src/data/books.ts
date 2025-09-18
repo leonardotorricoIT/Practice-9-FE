a0e8da3e0212217a0406fef2ca780e11b64d4988
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2025-09-18 12:37:37 -0400

    first commit

---
+++ src/data/books.ts
@@ -0,0 +1,22 @@
+import type { Book } from "../types/Book";
+
+export const books: Book[] = [
+  {
+    id: "1",
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    description: "A handbook of agile software craftsmanship.",
+  },
+  {
+    id: "2",
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt, David Thomas",
+    description: "Classic book with practical tips for programmers.",
+  },
+  {
+    id: "3",
+    title: "You Don’t Know JS",
+    author: "Kyle Simpson",
+    description: "Deep dive into JavaScript concepts.",
+  },
+];
